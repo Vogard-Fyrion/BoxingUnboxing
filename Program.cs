d2603cd03df9e2ad973b796a5b4e810a3e9138b3
Vogard-Fyrion <boggslev@yahoo.com>
2022-02-07 20:54:33 -0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> variables = new List<object>();
+            {
+                variables.Add(7);
+                variables.Add(28);
+                variables.Add(-1);
+                variables.Add(true);
+                variables.Add("chair");
+            }
+            int sum = 0;
+            foreach(var indx in variables)
+            {
+                if(indx is int)
+                {
+                    Console.WriteLine(indx);
+                    sum += (int)indx;
+                }
+                else
+                {
+                    Console.WriteLine(indx);
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
